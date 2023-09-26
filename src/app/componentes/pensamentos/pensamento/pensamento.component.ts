6ad7ca0df0bb3c2d71b2556a58217be5b449d8ec
Allan <allan5816@gmail.com>
2023-09-25 21:27:42 -0300

    enviando o projeto

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I started Angular',
+    autoria: 'Allan',
+    modelo: 'modelo3'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
